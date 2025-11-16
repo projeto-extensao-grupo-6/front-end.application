8a6fd597c9236c0793eaa6ba317c8cc9f3590f0a
ViniciusAlves-DEV <vsantos@f3capital.com.br>
2025-11-16 15:38:47 -0300

    integration almost done

--- src/services/agendamentosService.js
+++ src/services/agendamentosService.js
@@ -1,10 +1,6 @@
 const API_BASE_URL = "http://localhost:3000/api";
 
-<<<<<<< HEAD
-const agendamentosService = {
-=======
 export const agendamentosService = {
->>>>>>> 70ab83d (integrations)
   create: async (agendamento) => {
     try {
       const response = await fetch(`${API_BASE_URL}/agendamentos`, {
